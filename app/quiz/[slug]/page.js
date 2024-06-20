3ccdf770e7cd130e85ad37eb1d7da17037f99928
SoulSkunk <kylian.broccolichi@mediaschool.me>
2024-06-20 16:50:59 +0200

    Finished v0

--- app/quiz/[slug]/page.js
+++ app/quiz/[slug]/page.js
@@ -1,12 +1,35 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { useStore } from "@/store/store";
 import categoriesData from "@/data/categories.json";
 
 export default function QuizPage({ params }) {
+  ////////////////////////////////////////////////////////
+  //Stock les questions en fonction de la catégorie cliqué
   const [questions, setQuestions] = useState([]);
+
+  //Pagination des questions
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
+  //Stock la réponse choisi par l'user
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+
+  //Boolean isCorrect = true => good answer
+  const [isCorrect, setIsCorrect] = useState(null);
+
+  // Stocke le score
+  const [score, setScore] = useState(0);
+
+  // Stocke si la question a été répondue
+  const [isAnswered, setIsAnswered] = useState(false);
+
+  //////////////////////////////////////////////////
+  // Sélectionne 10 questions random
+  const selectRandomQuestions = (allQuestions) => {
+    const shuffled = allQuestions.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, 10);
+  };
+
+  //Fetch des questions en fonction de la categorie
   useEffect(() => {
     const fetchQuestions = async () => {
       try {
@@ -14,7 +37,8 @@ export default function QuizPage({ params }) {
           `https://quizzapi.jomoreschi.fr/api/v1/quiz?category=${params.slug}`
         );
         const data = await response.json();
-        setQuestions(data.quizzes);
+        const randomQuestions = selectRandomQuestions(data.quizzes);
+        setQuestions(randomQuestions);
       } catch (error) {
         console.error("Erreur lors de la récupération des questions :", error);
       }
@@ -25,49 +49,109 @@ export default function QuizPage({ params }) {
     }
   }, [params]);
 
+  //Fait spawn les prochaines questions
   const handleNextQuestion = () => {
+    if (isCorrect) {
+      setScore((prevScore) => prevScore + 1);
+    }
     setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+    setSelectedAnswer(null);
+    setIsCorrect(null);
+    setIsAnswered(false);
   };
 
+  //Fait spawn les précédentes questions
   const handlePreviousQuestion = () => {
     setCurrentQuestionIndex((prevIndex) => prevIndex - 1);
+    setSelectedAnswer(null);
+    setIsCorrect(null);
+    setIsAnswered(false);
   };
 
-  const getCategoryName = (slug) => {
+  // Gère la sélection d'une réponse
+  const handleAnswerClick = (answer) => {
+    if (isAnswered) return; // Empêche de sélectionner une autre réponse
+    setSelectedAnswer(answer);
+    const correct = answer === currentQuestion.answer;
+    setIsCorrect(correct);
+    setIsAnswered(true);
+  };
+
+  //On recup et stock la categorie choisi (via l'url) on find dans le json
+  const getCategoryInfo = (slug) => {
     const category = categoriesData.find((cat) => cat.slug === slug);
-    return category ? category.name : "";
+    return category ? { name: category.name, emoji: category.emoji } : {};
   };
 
+  //Si c'est long => loading
   if (questions.length === 0) {
-    return <div>Loading...</div>;
+    return <div className="text-center text-4xl">Loading...</div>;
   }
 
   const currentQuestion = questions[currentQuestionIndex];
 
+  const { name, emoji } = getCategoryInfo(params.slug);
+
   return (
-    <div className="max-w-lg mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">
-        Quiz - {getCategoryName(params.slug)}
-      </h1>
-      <div className="mb-8">
-        <h2 className="text-xl font-semibold mb-2">
-          Question {currentQuestionIndex + 1}: {currentQuestion.question}
-        </h2>
-        <ul>
-          {currentQuestion.badAnswers.map((answer, index) => (
-            <li key={index} className="ml-4 list-disc">
-              {answer}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="flex justify-between">
-        {currentQuestionIndex > 0 && (
-          <button onClick={handlePreviousQuestion}>Question précédente</button>
-        )}
-        {currentQuestionIndex < questions.length - 1 && (
-          <button onClick={handleNextQuestion}>Question suivante</button>
-        )}
+    <div className="flex items-center justify-center ">
+      <div className=" p-4">
+        <h1 className="text-5xl font-bold mb-4">
+          {name} {emoji}
+        </h1>
+        <div className="mb-8">
+          <h2 className="text-3xl font-semibold mb-2">
+            Question {currentQuestionIndex + 1} : {currentQuestion.question}
+          </h2>
+          <ul>
+            {currentQuestion.badAnswers
+              .concat(currentQuestion.answer)
+              .map((answer, index) => (
+                <li
+                  key={index}
+                  className={`ml-4 list-disc text-xl cursor-pointer ${
+                    selectedAnswer === answer
+                      ? isCorrect
+                        ? "text-green-500"
+                        : "text-red-500"
+                      : ""
+                  }`}
+                  onClick={() => handleAnswerClick(answer)}
+                >
+                  {answer}
+                </li>
+              ))}
+          </ul>
+        </div>
+        <div className="flex justify-between">
+          {currentQuestionIndex > 0 && (
+            <button
+              className="p-2 bg-teal-600 rounded-md"
+              onClick={handlePreviousQuestion}
+            >
+              Question précédente
+            </button>
+          )}
+          {currentQuestionIndex < questions.length - 1 && (
+            <button
+              className="p-2 bg-teal-600 rounded-md"
+              onClick={handleNextQuestion}
+            >
+              Question suivante
+            </button>
+          )}
+        </div>
+        <div className="pt-12">
+          <p className="text-slate-600">
+            Attention, une fois cliqué vous ne pourrez pas modifier votre
+            réponse
+          </p>
+
+          <br />
+          <p className="text-xl font-semibold ">
+            {" "}
+            Vous avez {score}/{questions.length} bonnes réponses
+          </p>
+        </div>
       </div>
     </div>
   );
--- components/Categories.js
+++ components/Categories.js
@@ -25,14 +25,14 @@ export default function Categories() {
 
   return (
     <div className="container mx-auto mt-8">
-      <h1 className="text-4xl font-bold text-center text-gray-800 mb-8">
+      <h1 className="text-4xl font-bold text-center mb-8">
         Choisis la catégorie du quizz
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {categories.map((category) => (
           <Link key={category.slug} href={`/quiz/${category.slug}`}>
             <div
-              className="cursor-pointer border rounded-lg p-4 text-center"
+              className="cursor-pointer border border-teal-600 rounded-lg p-4 text-center"
               onClick={() => handleCategorySelect(category)}
             >
               <div className="text-2xl">{category.emoji}</div>
